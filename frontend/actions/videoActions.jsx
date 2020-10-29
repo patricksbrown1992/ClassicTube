8d93e50dba4e979b8dc994201f7d04ec4d702af0
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-29 17:51:25 -0400

    added thunk actions

---
+++ frontend/actions/videoActions.jsx
@@ -0,0 +1,28 @@
+import * as VideoAPIUtil from "../util/videoUtil";
+
+export const RECEIVE_VIDEOS = "RECEIVE_VIDEOS";
+export const RECEIVE_VIDEO = "RECEIVE_VIDEO";
+
+const receiveVideos = (videos) => {
+  return {
+    type: RECEIVE_VIDEOS,
+    videos,
+  };
+};
+
+const receiveVideo = (video) => ({
+  type: RECEIVE_VIDEO,
+  video,
+});
+
+export const getVideos = () => (dispatch) => {
+  return VideoAPIUtil.getVideos().then((videos) => {
+    dispatch(receiveVideos(videos));
+  });
+};
+
+export const getVideo = (id) => (dispatch) => {
+  return VideoAPIUtil.getVideo(id).then((video) => {
+    dispatch(receiveVideo(video));
+  });
+};
