e2b5a2d7429ac43897ac943fcf7a8228ba5e7bb8
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-11-01 05:25:30 -0500

    added entities reducer

--- frontend/reducers/entities/entitiesReducer.jsx
+++ frontend/reducers/entities/entitiesReducer.jsx
@@ -1,10 +1,14 @@
 import usersReducer from "./usersReducer";
 import videoReducer from "./videoReducer";
 import { combineReducers } from "redux";
+import likesReducer from "./likesReducer";
+import dislikesReducer from "./dislikesReducer";
 
 const entitiesReducer = combineReducers({
   user: usersReducer,
   videos: videoReducer,
+  likes: likesReducer,
+  dislikes: dislikesReducer,
 });
 
 export default entitiesReducer;
