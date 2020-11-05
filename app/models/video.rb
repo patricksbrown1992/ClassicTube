300f976d31285ed53563f5447ea7db69dc4b4446
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-11-05 00:27:16 -0500

    video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,8 @@
 class Video < ApplicationRecord
-    validates :title, :runtime, :year, :user_id, :description, presence: true
+    validates :title, :runtime, :year, :views, :user_name, :likes, :dislikes, :user_id, :description, presence: true
     validates :title, uniqueness: true
     has_one_attached :movie
     has_one_attached :thumbnail
     belongs_to :user
+    
 end
